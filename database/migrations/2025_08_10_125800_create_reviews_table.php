b2dc7f468a247b86e5d78b2de19cc1f69f4e14a8
ahmad ibrahim <ahmadibrahimua18@gmail.com>
2025-08-10 16:02:21 +0300

    add the columns to the rebiews table

--- database/migrations/2025_08_10_125800_create_reviews_table.php
+++ database/migrations/2025_08_10_125800_create_reviews_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+ public function up()
+{
+    Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('content_creator_id')->constrained()->onDelete('cascade');
+        $table->integer('rating'); // 1 to 5
+        $table->text('comment')->nullable();
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
